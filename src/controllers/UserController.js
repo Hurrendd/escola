b6d762ca676e542087fb31e66c1f79588bca3646
Hurrendd <hurrendd.ramos@gmail.com>
2023-08-31 23:40:56 -0300

    Aula 176 - CRUD de Usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,80 @@ class UserController {
   async store(request, response) {
     try {
       const newUser = await User.create(request.body);
-      response.json(newUser);
+      return response.json(newUser);
     } catch (e) {
-      response.status(400).json({
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(request, response) {
+    try {
+      const allUsers = await User.findAll();
+      return response.json(allUsers);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(request, response) {
+    try {
+      const { id } = request.params;
+      const showOneUsers = await User.findByPk(id);
+      return response.json(showOneUsers);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(request, response) {
+    try {
+      const { id } = request.params;
+      if (!id) {
+        return response.status(400).json({
+          errors: ["ID não foi enviado."],
+        });
+      }
+      const showOneUsers = await User.findByPk(id);
+      if (!showOneUsers) {
+        return response.status(400).json({
+          errors: ["Usuário não existe."],
+        });
+      }
+
+      const newData = await showOneUsers.update(request.body);
+      return response.json(newData);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(request, response) {
+    try {
+      const { id } = request.params;
+      if (!id) {
+        return response.status(400).json({
+          errors: ["ID não foi enviado."],
+        });
+      }
+      const showOneUsers = await User.findByPk(id);
+      if (!showOneUsers) {
+        return response.status(400).json({
+          errors: ["Usuário não existe."],
+        });
+      }
+
+      await showOneUsers.destroy();
+      return response.status(202);
+    } catch (e) {
+      return response.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import usercontroller from "../controllers/UserController";
 const router = new Router();
 
 router.post("/", usercontroller.store);
+router.get("/", usercontroller.index);
+router.get("/:id", usercontroller.show);
+router.put("/:id", usercontroller.update);
+router.delete("/:id", usercontroller.delete);
 
 export default router;
